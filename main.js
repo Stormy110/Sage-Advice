664ac697eec67b9d03db8cc4c93629332d6ccbca
Anthony Meleah <anthony.meleah@gmail.com>
2020-11-17 10:51:33 -0500

    first commit

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 // Import Modules
-import ajax from "/modules/ajax.js";
-import { makeElements, getElements } from "/modules/elements.js";
+import ajax from "./modules/ajax.js";
+import { makeElements, getElements } from "./modules/elements.js";
+
+
 // API's
-// Body - Crosfit Gym's
-const apiKey = "Enter Here";
-const gyms = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-// Mind - Chuck Norris
+// Body - Chuck Norris
 const chuck = "https://api.chucknorris.io/jokes/random";
+
+// Mind - Advice slip
+const adslip = "https://api.adviceslip.com/advice";
+
 // Soul - Dog Pictures
 const dogPics = "https://random.dog/woof.json";
 
-ajax(gyms, (data) => {
-  console.log(data);
-});
+
 
 ajax(chuck, (data) => {
   console.log(data);
 });
 
+ajax(adslip, (data) => {
+  let r = JSON.parse(data);
+  console.log(r);
+});
+
 ajax(dogPics, (data) => {
   console.log(data);
-});
+})
+
